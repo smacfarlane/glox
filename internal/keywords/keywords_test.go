1fec2f425349a464d8786f416615046aa04b8b93
Brian Cain <bcain@hashicorp.com>
2022-03-24 10:30:01 -0700

    Add a missing key test to Keywords
    This commit also incldues an update where if a Keyword lookup was not
    found, we return an UNKNOWN token type, since TokenType is an int and
    the default value of int is 0.

--- internal/keywords/keywords_test.go
+++ internal/keywords/keywords_test.go
@@ -15,5 +15,21 @@ func TestKeyword(t *testing.T) {
 		tokenKey, ok := Keyword("and")
 		require.Equal(tokenKey, tokens.AND)
 		require.True(ok)
+
+		tokenKey, ok = Keyword("class")
+		require.Equal(tokenKey, tokens.CLASS)
+		require.True(ok)
+
+		tokenKey, ok = Keyword("else")
+		require.Equal(tokenKey, tokens.ELSE)
+		require.True(ok)
+	})
+
+	t.Run("Keyword returns false if key not exist", func(t *testing.T) {
+		require := require.New(t)
+
+		tokenKey, ok := Keyword("nope")
+		require.Equal(tokenKey, tokens.UNKNOWN)
+		require.False(ok)
 	})
 }
